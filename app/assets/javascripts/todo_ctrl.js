ab4e762f716eaff1f9591fb375117e514c10f25c
Terry Ramsdale <terrams@me.com>
2016-12-19 21:10:18 -0600

    class exercise

---
+++ app/assets/javascripts/todo_ctrl.js
@@ -0,0 +1,25 @@
+
+(function () {
+  "use strict";
+
+  angular.module("app").controller("todosCtrl", function($scope) {
+
+    $scope.firstTask = "Jump";
+    $scope.secondTask = "stand";
+    $scope.thirdTask = "sit";
+    $scope.tasks = ['one', 'two', 'three'];
+
+    $scope.addTask = function(inputText) {
+      if ($scope.newTask !== "" && $scope.newTask !== null && typeof $scope.newTask !== "undefined") {
+        $scope.tasks.push(inputText);
+        $scope.newTask = null;
+      }
+    };
+
+    $scope.deleteTask = function(deleteTask) {
+      var index = $scope.tasks.indexOf(deleteTask);
+      $scope.tasks.splice(index, 1);
+    };
+
+  });
+})();
